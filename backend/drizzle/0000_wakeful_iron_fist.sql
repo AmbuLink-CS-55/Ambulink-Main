54f969da324459d6e6ee029374c51599bfc96ca1
themiya kulathunga <tdeemanthe@gmail.com>
2026-02-02 10:29:26 +0530

    fix Docker errors

--- backend/drizzle/0000_wakeful_iron_fist.sql
+++ backend/drizzle/0000_wakeful_iron_fist.sql
@@ -1,4 +1,5 @@
-CREATE EXTENSION postgis;
+-- CREATE EXTENSION postgis;
+CREATE EXTENSION IF NOT EXISTS postgis;
 
 CREATE TYPE "public"."ambulance_status" AS ENUM('AVAILABLE', 'BUSY', 'OFFLINE');--> statement-breakpoint
 CREATE TYPE "public"."booking_status" AS ENUM('REQUESTED', 'ASSIGNED', 'ARRIVED', 'PICKEDUP', 'COMPLETED', 'CANCELLED');--> statement-breakpoint
