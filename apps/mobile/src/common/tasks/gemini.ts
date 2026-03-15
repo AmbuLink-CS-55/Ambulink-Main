c7b63fbc290656c75a32ec237c730bc8f3acd2fe
Nuwanka Fernando <w.s.nuwanka.fernando@gmail.com>
2026-03-15 15:30:33 +0530

    change the gemini model of the first aid api call

--- apps/mobile/src/common/tasks/gemini.ts
+++ apps/mobile/src/common/tasks/gemini.ts
@@ -7,7 +7,7 @@ export const getFirstAidResponse = async (
 ): Promise<string> => {
   try {
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`,
       {
         contents: [
           {
